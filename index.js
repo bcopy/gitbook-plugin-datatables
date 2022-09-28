6ac98935abadfd48ea191ef5478744b870980adb
Brice Copy <500789+bcopy@users.noreply.github.com>
2022-09-28 09:50:38 +0200

    Update table attributes and append script calls

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
+const cheerio = require('cheerio');
+const crypto = require('crypto');
+
 module.exports = {
     hooks: {
       init: async function () {
-        console.warn("Starting datatabs plugin...")
+        console.warn("Starting Datatables plugin...");
       },
       "page": function(page) {
         // Inspect page contents and initialize all tables that carry the datatables class
-        console.warn("Invoking page hook ... on "+page.content)
-        page.content = page.content.replace('class="datatable"','class="datatable" data-processed="true"')
-        console.warn("new content : "+page.content)
+        const $ = cheerio.load(page.content, null, false);
+
+        var tableIds = []
+        
+        // Iterate around all the tables marked as datatables and collect their element id
+        $('table[class=datatable]').each(function( index ) {
+          console.debug("Processing table "+index);
+          if (  $(this).attr('id') === undefined ){
+            $(this).attr('id',`datatable-${crypto.randomBytes(16).toString('base64').substring(0, 16)}`);
+            console.warn("Set table "+index+" id to "+$(this).attr('id'));
+          }
+          $(this).attr("data-processed", "true");
+          tableIds.push($(this).attr('id'));
+        });
+
+        page.content = $.html();
+
+        // Wrap all datatables in constructor calls in a script at the bottom of the page
+        if(tableIds.length > 0){
+          page.content += '\n\n<script type="text/javascript">\n';
+          tableIds.forEach(function(value){
+            console.warn("constructor for "+value);
+            page.content += "new DataTable('#"+value+"',{});\n";
+          });
+          page.content += '</script>\n\n';
+        }
+
+        console.warn("new content : "+page.content);
         return page;
       },
     }
