9e1a2b622ff5ff324a7f018e919ad67b748fc4f2
Brice Copy <500789+bcopy@users.noreply.github.com>
2022-09-29 16:39:41 +0200

    Add datatables extensions

--- index.js
+++ index.js
@@ -4,8 +4,36 @@ const crypto = require('crypto');
 module.exports = {
     book:{
       assets: './assets',
-      js: [ 'js/jquery.min.js','js/jquery.dataTables.min.js', 'js/dataTables.dataTables.min.js','js/plugin.js'],
-      css: [ 'css/jquery.dataTables.min.css']
+      js: [ 'js/jquery.min.js'
+           ,'js/jquery.dataTables.min.js'
+           ,'js/dataTables.dataTables.min.js'
+           ,'js/colReorder.dataTables.min.js'
+           ,'js/dataTables.colReorder.min.js'
+           ,'js/fixedColumns.dataTables.min.js'
+           ,'js/dataTables.fixedColumns.min.js'
+           ,'js/fixedHeader.dataTables.min.js'
+           ,'js/dataTables.fixedHeader.min.js'
+           ,'js/responsive.dataTables.min.js'
+           ,'js/dataTables.responsive.min.js'
+           ,'js/rowGroup.dataTables.min.js'
+           ,'js/dataTables.rowGroup.min.js'
+           ,'js/searchBuilder.dataTables.min.js'
+           ,'js/dataTables.searchBuilder.min.js'
+           ,'js/select.dataTables.min.js'
+           ,'js/dataTables.select.min.js'   
+           ,'js/searchPanes.dataTables.min.js'
+           ,'js/dataTables.searchPanes.min.js'
+          ,'js/plugin.js'],
+      css: [ 'css/jquery.dataTables.min.css'
+            ,'css/colReorder.dataTables.min.css'
+            ,'css/fixedColumns.dataTables.min.css'
+            ,'css/fixedHeader.dataTables.min.css'
+            ,'css/responsive.dataTables.min.css'
+            ,'css/rowGroup.dataTables.min.css'
+            ,'css/searchBuilder.dataTables.min.css'
+            ,'css/searchPanes.dataTables.min.css'
+            ,'css/select.dataTables.min.css'
+      ]
     },
     hooks: {
       init: async function () {
